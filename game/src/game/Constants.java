b89d1331b6d216b707c2d6813413969f6fd8286c
Jesse Huang <huangfamilyca@gmail.com>
2019-05-29 18:31:19 -0700

    Deleting Unnecessary Game and .metadata Folders

--- game/src/game/Constants.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class Constants {
-	public static double GRAVITY = 0.08;
-	public static int GAME_WIDTH = 1200;
-	public static int GAME_HEIGHT = 600;
-	public static int JUMP_VELOCITY = -4;
-	public static int MOVE_VELOCITY = 1;
-}
